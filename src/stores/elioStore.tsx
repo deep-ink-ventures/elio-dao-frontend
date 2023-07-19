5b349b3e492cea9ef18447527f46183c8522a3b8
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-07-19 18:22:40 +0100

    feat: add store refactor

--- src/stores/elioStore.tsx
+++ src/stores/elioStore.tsx
@@ -6,12 +6,24 @@ import {
 import type BigNumber from 'bignumber.js';
 import * as SorobanClient from 'soroban-client';
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
+
 import {
   NETWORK,
   NETWORK_PASSPHRASE,
   SERVICE_URL,
   SOROBAN_RPC_ENDPOINT,
 } from '../config/index';
+
+import type { AccountSlice } from './account';
+import { createAccountSlice } from './account';
+import type { DaoSlice } from './dao';
+import { createDaoSlice } from './dao';
+
+interface PageSlices {
+  dao: DaoSlice;
+  account: AccountSlice;
+}
 
 export const errorCodeMessages: ErrorCodeMessages = {
   1: 'DaoAlreadyExists',
@@ -200,6 +212,7 @@ export interface WalletAccount {
 }
 
 export interface ElioState {
+  pages: PageSlices;
   currentDao: DaoDetail | null;
   currentDaoFromChain: DaoDetail | null;
   daos: DaoDetail[] | null;
@@ -257,249 +270,257 @@ export interface ElioActions {
 
 export interface ElioStore extends ElioState, ElioActions {}
 
-const useElioStore = create<ElioStore>()((set, get) => ({
-  currentDao: null,
-  currentDaoFromChain: null,
-  currentWalletAccount: null,
-  isConnectModalOpen: false,
-  isTxnProcessing: false,
-  daoPage: 'dashboard',
-  isStartModalOpen: false,
-  createDaoSteps: 1,
-  daos: null,
-  txnNotifications: [],
-  currentProposals: null,
-  proposalCreationValues: null,
-  daoTokenBalance: null,
-  isFaultyModalOpen: false,
-  isFaultyReportsOpen: false,
-  currentProposalFaultyReports: null,
-  sorobanServer: new SorobanClient.Server(SOROBAN_RPC_ENDPOINT),
-  networkPassphrase: NETWORK_PASSPHRASE,
-  network: NETWORK,
-  showCongrats: false,
-  currentProposal: null,
-  currentBlockNumber: null,
-  updateCurrentDao: (currentDao) => set({ currentDao }),
-  updateIsConnectModalOpen: (isConnectModalOpen) => set({ isConnectModalOpen }),
-  updateIsTxnProcessing: (isTxnProcessing) => set({ isTxnProcessing }),
-  updateCurrentWalletAccount: (currentWalletAccount) =>
-    set({ currentWalletAccount }),
-  updateDaoPage: (daoPage) => set(() => ({ daoPage })),
-  updateIsStartModalOpen: (isStartModalOpen) =>
-    set(() => ({ isStartModalOpen })),
-  handleErrors: (errMsg: string, err?: Error | string) => {
-    set({ isTxnProcessing: false });
-    // eslint-disable-next-line
-    console.log(errMsg, err);
-    let message = '';
-
-    if (typeof err === 'object') {
-      message = err.message;
-    } else {
-      message = errMsg;
-    }
-
-    const getErrorCode = (str: string | undefined) => {
-      if (typeof str === 'undefined') {
-        return null;
-      }
-      const match = str.match(/\((\d+)\)/);
-      if (!match?.[1]) {
-        return null;
-      }
+const useElioStore = create<ElioStore>()(
+  devtools((set, get, store) => ({
+    currentDao: null,
+    currentDaoFromChain: null,
+    currentWalletAccount: null,
+    isConnectModalOpen: false,
+    isTxnProcessing: false,
+    daoPage: 'dashboard',
+    isStartModalOpen: false,
+    createDaoSteps: 1,
+    daos: null,
+    txnNotifications: [],
+    currentProposals: null,
+    proposalCreationValues: null,
+    daoTokenBalance: null,
+    isFaultyModalOpen: false,
+    isFaultyReportsOpen: false,
+    currentProposalFaultyReports: null,
+    sorobanServer: new SorobanClient.Server(SOROBAN_RPC_ENDPOINT),
+    networkPassphrase: NETWORK_PASSPHRASE,
+    network: NETWORK,
+    showCongrats: false,
+    currentProposal: null,
+    currentBlockNumber: null,
+    updateCurrentDao: (currentDao) => set({ currentDao }),
+    updateIsConnectModalOpen: (isConnectModalOpen) =>
+      set({ isConnectModalOpen }),
+    updateIsTxnProcessing: (isTxnProcessing) => set({ isTxnProcessing }),
+    updateCurrentWalletAccount: (currentWalletAccount) =>
+      set({ currentWalletAccount }),
+    updateDaoPage: (daoPage) => set(() => ({ daoPage })),
+    updateIsStartModalOpen: (isStartModalOpen) =>
+      set(() => ({ isStartModalOpen })),
+    handleErrors: (errMsg: string, err?: Error | string) => {
+      set({ isTxnProcessing: false });
       // eslint-disable-next-line
-      return match ? parseInt(match[1]) : null;
-    };
+    console.log(errMsg, err);
+      let message = '';
 
-    if (typeof err === 'string' && err.includes('ContractError(')) {
-      const errorCode = getErrorCode(err);
-      if (errorCode && errorCodeMessages[errorCode]) {
-        if (typeof err === 'object') {
-          message = errorCodeMessages[errorCode] as string;
-        } else {
-          message = `${errorCodeMessages[errorCode] as string} - ${message}`;
+      if (typeof err === 'object') {
+        message = err.message;
+      } else {
+        message = errMsg;
+      }
+
+      const getErrorCode = (str: string | undefined) => {
+        if (typeof str === 'undefined') {
+          return null;
+        }
+        const match = str.match(/\((\d+)\)/);
+        if (!match?.[1]) {
+          return null;
+        }
+        // eslint-disable-next-line
+      return match ? parseInt(match[1]) : null;
+      };
+
+      if (typeof err === 'string' && err.includes('ContractError(')) {
+        const errorCode = getErrorCode(err);
+        if (errorCode && errorCodeMessages[errorCode]) {
+          if (typeof err === 'object') {
+            message = errorCodeMessages[errorCode] as string;
+          } else {
+            message = `${errorCodeMessages[errorCode] as string} - ${message}`;
+          }
         }
       }
-    }
 
-    const newNoti = {
-      title: TxnResponse.Error,
-      message,
-      type: TxnResponse.Error,
-      timestamp: Date.now(),
-    };
+      const newNoti = {
+        title: TxnResponse.Error,
+        message,
+        type: TxnResponse.Error,
+        timestamp: Date.now(),
+      };
 
-    get().addTxnNotification(newNoti);
-  },
-  handleTxnSuccessNotification(txnResponse, successMsg) {
-    // we don't turn off txnIsProcessing here
-    if (txnResponse.status !== 'SUCCESS') {
-      return;
-    }
+      get().addTxnNotification(newNoti);
+    },
+    handleTxnSuccessNotification(txnResponse, successMsg) {
+      // we don't turn off txnIsProcessing here
+      if (txnResponse.status !== 'SUCCESS') {
+        return;
+      }
 
-    const noti = {
-      title: TxnResponse.Success,
-      message: successMsg,
-      type: TxnResponse.Success,
-      timestamp: Date.now(),
-      // txnHash?: string;
-    };
-    get().addTxnNotification(noti);
-  },
-  addTxnNotification: (newNotification) => {
-    const oldTxnNotis = get().txnNotifications;
-    // add the new noti to first index because we will start displaying notis from the last index
-    const newNotis = [newNotification, ...oldTxnNotis];
-    set({ txnNotifications: newNotis });
-  },
-  removeTxnNotification: () => {
-    // first in first out
-    const currentTxnNotis = get().txnNotifications;
-    const newNotis = currentTxnNotis.slice(0, -1);
-    set({ txnNotifications: newNotis });
-  },
-  updateCreateDaoSteps: (createDaoSteps) => set({ createDaoSteps }),
-  updateProposalCreationValues: (proposalCreationValues) =>
-    set({ proposalCreationValues }),
-  updateIsFaultyModalOpen: (isFaultyModalOpen) => set({ isFaultyModalOpen }),
-  updateIsFaultyReportsOpen: (isFaultyReportsOpen) =>
-    set({ isFaultyReportsOpen }),
-  getWallet: async () => {
-    // wallet is automatically injected to the window we just need to get the values
-    const connected = await isConnected();
-    const networkDetails = await getNetworkDetails();
-    const publicKey = await getPublicKey();
-    const wallet: WalletAccount = {
-      isConnected: connected,
-      network: networkDetails.network,
-      networkPassphrase: networkDetails.networkPassphrase,
-      publicKey,
-      networkUrl: networkDetails.networkUrl,
-    };
-    set({ currentWalletAccount: wallet });
-  },
-  fetchDaosDB: async () => {
-    try {
-      const getDaosResponse = await fetch(
-        `${SERVICE_URL}/daos/?order_by=id&limit=100`
-      );
-      const daosRes = await getDaosResponse.json();
-      const daosArr = daosRes.results;
-      const newDaos: DaoDetail[] = daosArr?.map((dao: any) => {
-        return {
-          daoId: dao.id,
-          daoName: dao.name,
-          daoAssetId: dao.asset_id,
-          daoOwnerAddress: dao.owner_id,
-          daoCreatorAddress: dao.creator_id,
-          setupComplete: dao.setup_complete,
-          metadataUrl: dao.metadata_url,
-          metadataHash: dao.metadata_hash,
-          email: dao.metadata?.email || null,
-          descriptionShort: dao.metadata?.description_short || null,
-          descriptionLong: dao.metadata?.description_long || null,
+      const noti = {
+        title: TxnResponse.Success,
+        message: successMsg,
+        type: TxnResponse.Success,
+        timestamp: Date.now(),
+        // txnHash?: string;
+      };
+      get().addTxnNotification(noti);
+    },
+    addTxnNotification: (newNotification) => {
+      const oldTxnNotis = get().txnNotifications;
+      // add the new noti to first index because we will start displaying notis from the last index
+      const newNotis = [newNotification, ...oldTxnNotis];
+      set({ txnNotifications: newNotis });
+    },
+    removeTxnNotification: () => {
+      // first in first out
+      const currentTxnNotis = get().txnNotifications;
+      const newNotis = currentTxnNotis.slice(0, -1);
+      set({ txnNotifications: newNotis });
+    },
+    updateCreateDaoSteps: (createDaoSteps) => set({ createDaoSteps }),
+    updateProposalCreationValues: (proposalCreationValues) =>
+      set({ proposalCreationValues }),
+    updateIsFaultyModalOpen: (isFaultyModalOpen) => set({ isFaultyModalOpen }),
+    updateIsFaultyReportsOpen: (isFaultyReportsOpen) =>
+      set({ isFaultyReportsOpen }),
+    getWallet: async () => {
+      // wallet is automatically injected to the window we just need to get the values
+      const connected = await isConnected();
+      const networkDetails = await getNetworkDetails();
+      const publicKey = await getPublicKey();
+      const wallet: WalletAccount = {
+        isConnected: connected,
+        network: networkDetails.network,
+        networkPassphrase: networkDetails.networkPassphrase,
+        publicKey,
+        networkUrl: networkDetails.networkUrl,
+      };
+      set({ currentWalletAccount: wallet });
+    },
+    fetchDaosDB: async () => {
+      try {
+        const getDaosResponse = await fetch(
+          `${SERVICE_URL}/daos/?order_by=id&limit=100`
+        );
+        const daosRes = await getDaosResponse.json();
+        const daosArr = daosRes.results;
+        const newDaos: DaoDetail[] = daosArr?.map((dao: any) => {
+          return {
+            daoId: dao.id,
+            daoName: dao.name,
+            daoAssetId: dao.asset_id,
+            daoOwnerAddress: dao.owner_id,
+            daoCreatorAddress: dao.creator_id,
+            setupComplete: dao.setup_complete,
+            metadataUrl: dao.metadata_url,
+            metadataHash: dao.metadata_hash,
+            email: dao.metadata?.email || null,
+            descriptionShort: dao.metadata?.description_short || null,
+            descriptionLong: dao.metadata?.description_long || null,
+            images: {
+              contentType: dao.metadata?.images.logo.content_type || null,
+              small: dao.metadata?.images.logo.small.url || null,
+              medium: dao.metadata?.images.logo.medium.url || null,
+              large: dao.metadata?.images.logo.medium.url || null,
+            },
+          };
+        });
+        set({ daos: newDaos });
+      } catch (err) {
+        get().handleErrors(err);
+      }
+    },
+    fetchDaoDB: async (daoId: string) => {
+      try {
+        const daoDetail: DaoDetail = {
+          daoId: '{N/A}',
+          daoName: '{N/A}',
+          daoOwnerAddress: '{N/A}',
+          daoCreatorAddress: '{N/A}',
+          setupComplete: false,
+          proposalDuration: null,
+          proposalTokenDeposit: 0,
+          minimumMajority: null,
+          daoAssetId: null,
+          metadataUrl: null,
+          metadataHash: null,
+          descriptionShort: null,
+          descriptionLong: null,
+          email: null,
           images: {
-            contentType: dao.metadata?.images.logo.content_type || null,
-            small: dao.metadata?.images.logo.small.url || null,
-            medium: dao.metadata?.images.logo.medium.url || null,
-            large: dao.metadata?.images.logo.medium.url || null,
+            contentType: null,
+            small: null,
+            medium: null,
+            large: null,
           },
         };
-      });
-      set({ daos: newDaos });
-    } catch (err) {
-      get().handleErrors(err);
-    }
-  },
-  fetchDaoDB: async (daoId: string) => {
-    try {
-      const daoDetail: DaoDetail = {
-        daoId: '{N/A}',
-        daoName: '{N/A}',
-        daoOwnerAddress: '{N/A}',
-        daoCreatorAddress: '{N/A}',
-        setupComplete: false,
-        proposalDuration: null,
-        proposalTokenDeposit: 0,
-        minimumMajority: null,
-        daoAssetId: null,
-        metadataUrl: null,
-        metadataHash: null,
-        descriptionShort: null,
-        descriptionLong: null,
-        email: null,
-        images: {
-          contentType: null,
-          small: null,
-          medium: null,
-          large: null,
-        },
-      };
-      const response = await fetch(
-        `${SERVICE_URL}/daos/${encodeURIComponent(daoId as string)}/`
-      );
-      if (response.status === 404) {
-        return;
-      }
-      const d = await response.json();
-      daoDetail.daoId = d.id;
-      daoDetail.daoName = d.name;
-      daoDetail.daoAssetId = d.asset_id;
-      daoDetail.daoOwnerAddress = d.owner_id;
-      daoDetail.daoCreatorAddress = d.creator_id;
-      daoDetail.proposalDuration = d.proposal_duration;
-      daoDetail.proposalTokenDeposit = d.proposal_token_deposit;
-      daoDetail.minimumMajority = d.minimum_majority_per_1024;
-      daoDetail.metadataUrl = d.metadata_url;
-      daoDetail.metadataHash = d.metadata_hash;
-      daoDetail.setupComplete = d.setup_complete;
-
-      if (d.metadata) {
-        daoDetail.descriptionShort = d.metadata.description_short;
-        daoDetail.descriptionLong = d.metadata.description_long;
-        daoDetail.email = d.metadata.email;
-        daoDetail.images.contentType = d.metadata.images.logo.content_type;
-        daoDetail.images.small = d.metadata.images.logo.small.url;
-        daoDetail.images.medium = d.metadata.images.logo.medium.url;
-        daoDetail.images.large = d.metadata.images.logo.large.url;
-      }
-
-      get().updateCurrentDao(daoDetail);
-    } catch (err) {
-      get().handleErrors(err);
-    }
-  },
-  updateShowCongrats: (showCongrats) => set({ showCongrats }),
-  updateDaoFromChain: (currentDaoFromChain: DaoDetail) =>
-    set({ currentDaoFromChain }),
-  updateCurrentBlockNumber: (currentBlockNumber) => set({ currentBlockNumber }),
-  fetchProposalFaultyReports: async (proposalId: string) => {
-    try {
-      const response = await fetch(
-        `${SERVICE_URL}/proposals/${proposalId}/reports/`
-      );
-
-      const reportsRes = await response.json();
-
-      if (!reportsRes || reportsRes.length < 1) {
-        return;
-      }
-
-      const reports = reportsRes?.map(
-        (item: { proposal_id: string; reason: string }) => {
-          return {
-            proposalId: item.proposal_id,
-            reason: item.reason,
-          };
+        const response = await fetch(
+          `${SERVICE_URL}/daos/${encodeURIComponent(daoId as string)}/`
+        );
+        if (response.status === 404) {
+          return;
         }
-      );
+        const d = await response.json();
+        daoDetail.daoId = d.id;
+        daoDetail.daoName = d.name;
+        daoDetail.daoAssetId = d.asset_id;
+        daoDetail.daoOwnerAddress = d.owner_id;
+        daoDetail.daoCreatorAddress = d.creator_id;
+        daoDetail.proposalDuration = d.proposal_duration;
+        daoDetail.proposalTokenDeposit = d.proposal_token_deposit;
+        daoDetail.minimumMajority = d.minimum_majority_per_1024;
+        daoDetail.metadataUrl = d.metadata_url;
+        daoDetail.metadataHash = d.metadata_hash;
+        daoDetail.setupComplete = d.setup_complete;
 
-      set({ currentProposalFaultyReports: reports });
-    } catch (err) {
-      get().handleErrors(err);
-    }
-  },
-}));
+        if (d.metadata) {
+          daoDetail.descriptionShort = d.metadata.description_short;
+          daoDetail.descriptionLong = d.metadata.description_long;
+          daoDetail.email = d.metadata.email;
+          daoDetail.images.contentType = d.metadata.images.logo.content_type;
+          daoDetail.images.small = d.metadata.images.logo.small.url;
+          daoDetail.images.medium = d.metadata.images.logo.medium.url;
+          daoDetail.images.large = d.metadata.images.logo.large.url;
+        }
+
+        get().updateCurrentDao(daoDetail);
+      } catch (err) {
+        get().handleErrors(err);
+      }
+    },
+    updateShowCongrats: (showCongrats) => set({ showCongrats }),
+    updateDaoFromChain: (currentDaoFromChain: DaoDetail) =>
+      set({ currentDaoFromChain }),
+    updateCurrentBlockNumber: (currentBlockNumber) =>
+      set({ currentBlockNumber }),
+    fetchProposalFaultyReports: async (proposalId: string) => {
+      try {
+        const response = await fetch(
+          `${SERVICE_URL}/proposals/${proposalId}/reports/`
+        );
+
+        const reportsRes = await response.json();
+
+        if (!reportsRes || reportsRes.length < 1) {
+          return;
+        }
+
+        const reports = reportsRes?.map(
+          (item: { proposal_id: string; reason: string }) => {
+            return {
+              proposalId: item.proposal_id,
+              reason: item.reason,
+            };
+          }
+        );
+
+        set({ currentProposalFaultyReports: reports });
+      } catch (err) {
+        get().handleErrors(err);
+      }
+    },
+    pages: {
+      ...createDaoSlice(set, get, store),
+      ...createAccountSlice(set, get, store),
+    },
+  }))
+);
 
 export default useElioStore;
