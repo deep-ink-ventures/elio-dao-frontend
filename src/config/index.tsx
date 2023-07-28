b0952307f651947a3e97ac8602dd76d280018df5
GHesericsu <60202455+GHesericsu@users.noreply.github.com>
2023-07-28 01:30:44 +0100

    docs: add readme

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -3,11 +3,11 @@ export const BLOCK_TIME: Readonly<number> = 6; // seconds
 export const XLM_UNITS: Readonly<number> = 10000000; // 7 decimals
 
 export const NETWORK_PASSPHRASE: Readonly<string> =
-  'Test SDF Future Network ; October 2022';
+  'Standalone Network ; February 2017';
 export const NETWORK: Readonly<string> = 'FUTURENET';
 export const BASE_FEE: Readonly<string> = '100';
 
 export const SOROBAN_RPC_ENDPOINT: Readonly<string> =
-  process.env.RPC_ENDPOINT || 'https://rpc-futurenet.stellar.org/';
+  process.env.RPC_ENDPOINT || 'https://node.elio-dao.org/soroban/rpc/';
 export const SERVICE_URL: Readonly<string> =
   process.env.SERVICE_URL || 'https://service.elio-dao.org';
