039dbdaa7fba765331dceba9c40461b43251d739
joh@deep-ink.ventures <joh@deep-ink.ventures>
2023-08-11 20:25:58 +0800

    feat(daocard): fetch dao token balance for each dao to display owned token indicator

--- src/components/DaoCard.tsx
+++ src/components/DaoCard.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { DaoService } from '@/services/daos';
 import useElioStore from '@/stores/elioStore';
 import mountain from '@/svg/mountain.svg';
 import placeholderImage from '@/svg/placeholderImage.svg';
+import type BigNumber from 'bignumber.js';
 import cn from 'classnames';
+import { useEffect, useState } from 'react';
 
 interface DaoCardProps {
   daoId: string;
@@ -16,14 +19,32 @@ interface DaoCardProps {
 }
 
 const DaoCard = (props: DaoCardProps) => {
+  const { daoName, daoId, daoOwnerAddress, imageUrl } = props;
   const [currentWalletAccount] = useElioStore((s) => [s.currentWalletAccount]);
+  const [daoBalance, setDaoBalance] = useState<BigNumber>();
 
-  const isOwner =
-    currentWalletAccount?.publicKey.toLowerCase() ===
-    props.daoOwnerAddress.toLowerCase();
+  const fetchDaoBalance = async () => {
+    if (currentWalletAccount?.publicKey) {
+      const balance = await DaoService.getBalance(
+        daoId,
+        currentWalletAccount.publicKey
+      );
+
+      if (balance) {
+        setDaoBalance(balance);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchDaoBalance();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentWalletAccount, daoId]);
+
+  const hasOwnedTokens = daoBalance && !daoBalance?.isZero();
 
   const displayImage = () => {
-    if (!props.imageUrl) {
+    if (!imageUrl) {
       return (
         <>
           <Image
@@ -44,8 +65,8 @@ const DaoCard = (props: DaoCardProps) => {
     return (
       <>
         <img
-          src={props.imageUrl}
-          alt={`${props.daoName} logo image`}
+          src={imageUrl}
+          alt={`${daoName} logo image`}
           height={60}
           width={60}
           className='rounded-full'
@@ -57,8 +78,8 @@ const DaoCard = (props: DaoCardProps) => {
   return (
     <div
       className={`card-compact relative z-0 m-1 w-64 py-4  shadow-xl hover:cursor-pointer md:w-56 md:pb-10 md:pt-4`}>
-      <Link href={`/dao/${encodeURIComponent(props.daoId)}`}>
-        {currentWalletAccount?.publicKey === props.daoOwnerAddress ? (
+      <Link href={`/dao/${encodeURIComponent(daoId)}`}>
+        {currentWalletAccount?.publicKey === daoOwnerAddress ? (
           <div className='absolute left-44 top-3 rounded-[15px] bg-primary px-2 py-1 text-xs md:left-40 md:top-3 md:block'>
             admin
           </div>
@@ -68,8 +89,8 @@ const DaoCard = (props: DaoCardProps) => {
             <div
               className={cn('rounded-full p-1', {
                 'bg-gradient-to-t from-pink-500 via-red-500 to-yellow-500':
-                  isOwner,
-                'bg-[#403945]': !isOwner,
+                  hasOwnedTokens,
+                'bg-[#403945]': !hasOwnedTokens,
               })}>
               <div className='relative rounded-full bg-[#403945] p-1'>
                 {displayImage()}
@@ -81,16 +102,16 @@ const DaoCard = (props: DaoCardProps) => {
               className={cn(
                 `z-10 w-[150px] break-words text-base-content mix-blend-normal md:h-12`,
                 {
-                  'text-sm': props.daoName?.length > 26,
+                  'text-sm': daoName?.length > 26,
                 }
               )}>
-              {props.daoName}
+              {daoName}
             </h4>
           </div>
-          <p className='text-sm text-accent'>{`DAO ID: ${props.daoId}`}</p>
+          <p className='text-sm text-accent'>{`DAO ID: ${daoId}`}</p>
         </div>
       </Link>
-      <Link href={`/dao/${encodeURIComponent(props.daoId)}`}>
+      <Link href={`/dao/${encodeURIComponent(daoId)}`}>
         <p className='absolute bottom-[10%] left-[33%] mt-5 hidden text-sm underline underline-offset-2 md:block'>{`See more >`}</p>
       </Link>
     </div>
